b4a740757f32f5fe0017dd4ee0b41304eef41eba
nissara <nissara.sup@thaivivat.co.th>
2019-10-01 21:56:05 +0700

    [ADD] page

--- src/component/Table.js
+++ src/component/Table.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
-import {Container, Table, Form, Button, Col, Row } from 'react-bootstrap'
+import {Container, Table, Form, Button, Col, Row, Pagination } from 'react-bootstrap'
 import List from './List'
 import EditComponet from './EditComponent'
 
 class TableUser extends Component {
     
+    handlePage = (event) => {
+        event.preventDefault()
+        console.log('tets')
+        console.log(this.props.users)
+        
+    }
   
     render () {
+    
+       
+        const countPage = this.props.users.length / 3 
+        console.log(countPage)
        
         return (
             <Container>
@@ -20,6 +30,21 @@ class TableUser extends Component {
                     </Col>
                     <Col sm={2}>
                         <Button variant="secondary" size="sm">Delete</Button>
+                    </Col>
+                    <Col sm={8}>
+                       
+                        <Pagination className="p-right">
+                            <Pagination.Prev />
+                            {(() => {
+                                const options = []
+                                for (let i = 1; i <= countPage ; i++) {
+                                    options.push(<Pagination.Item onClick={this.handlePage}>{i}</Pagination.Item>);
+                                }
+                                return options
+                            })()}
+                            {/* <Pagination.Item active>{12}</Pagination.Item> */}
+                            <Pagination.Next />
+                        </Pagination>
                     </Col>
                 </Row>
                 
